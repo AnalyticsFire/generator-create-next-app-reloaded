51acbba661f0ab5f79e7d059df4ffab7a03038fe
Alessander Franca <alessander70@gmail.com>
2018-06-12 13:03:31 -0300

    apollo local state example added

--- generators/app/templates/src/apollo/init-apollo.js
+++ generators/app/templates/src/apollo/init-apollo.js
@@ -1,5 +1,10 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink } from 'apollo-link';
+import gql from 'graphql-tag';
 
 let apolloClient = null;
 
@@ -8,12 +13,71 @@ if (!process.browser) {
   global.fetch = fetch;
 }
 
+const cache = new InMemoryCache();
+
+const defaultState = {
+  counter: {
+    __typename: 'Counter',
+    value: 0,
+  },
+};
+
+const query = gql`
+  query getCounter {
+    counter @client {
+      value
+    }
+  }
+`;
+
+const stateLink = withClientState({
+  cache,
+  defaults: defaultState,
+  resolvers: {
+    Mutation: {
+      increaseCounter: (_, { counter }, { cache }) => {
+        const previousState = cache.readQuery({ query });
+
+        const data = {
+          ...previousState,
+          counter: {
+            ...previousState.counter,
+            value: counter + 1,
+          },
+        };
+
+        cache.writeQuery({ query, data });
+        return counter;
+      },
+      decreaseCounter: (_, { counter }, { cache }) => {
+        const previousState = cache.readQuery({ query });
+
+        const data = {
+          ...previousState,
+          counter: {
+            ...previousState.counter,
+            value: counter > 0 ? counter - 1 : 0,
+          },
+        };
+
+        cache.writeQuery({ query, data });
+        return counter;
+      },
+    },
+  },
+});
+
 const create = initialState =>
   new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-    uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
-    cache: new InMemoryCache().restore(initialState || {}),
+    link: ApolloLink.from([
+      stateLink,
+      new HttpLink({
+        uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+      }),
+    ]),
+    cache,
   });
 
 export default function initApollo(initialState) {
